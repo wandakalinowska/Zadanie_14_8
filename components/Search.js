beaf8f009d70f5af3397879bc9099c9fdd638115
Wanda Kalinowska <wanda0501@gmail.com>
2018-03-22 18:41:25 +0100

    Change script

--- components/Search.js
+++ components/Search.js
@@ -12,7 +12,7 @@ Search = React.createClass({
         if (searchingText.length > 2) {
             this.props.onSearch(searchingText);
         }
-    },
+      },
 
     handleKeyUp: function(event) {
         if (event.keyCode === 13) {
@@ -23,12 +23,13 @@ Search = React.createClass({
     render: function() {
         var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 
-    return <input
-             type="text"
-             onChange={this.handleChange}
-             onKeyUp={this.handleKeyUp}
-             placeholder="Tutaj wpisz wyszukiwaną frazę"
-             style={styles}
-             value={this.state.searchTerm}
-            />
-  }
+    	return (<input
+            type="text"
+            onChange={this.handleChange}
+            onKeyUp={this.handleKeyUp}
+            placeholder="Tutaj wpisz wyszukiwaną frazę"
+            style={styles}
+            value={this.state.searchTerm}
+        />)
+    }
+});
