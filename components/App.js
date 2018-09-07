852df952f130a239acd8d5572fd231bfe12047f2
Wanda Kalinowska <wanda0501@gmail.com>
2018-09-07 23:46:06 +0200

    Bug fixed

--- components/App.js
+++ components/App.js
@@ -53,7 +53,7 @@ App = React.createClass({
         return (
           <div style={styles}>
                 <h1>Wyszukiwarka GIFów!</h1>
-                <p>Znajdź gifa na <a href='http://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać następny GIF.</p>
+                <p>Znajdź gifa na <a href='https://giphy.com'>giphy</a>. Naciskaj enter, aby pobrać następny GIF.</p>
                 <Search onSearch={this.handleSearch}/>
             <Gif 
                 loading={this.state.loading}
